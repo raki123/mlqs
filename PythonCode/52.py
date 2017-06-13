7de10deafdea42dc55713d38ab4b541ce208ad9f
VMCJ <michael.vasseur@gmail.com>
2017-06-13 23:24:33 +0200

    does this work

---
+++ PythonCode/52.py
@@ -0,0 +1,143 @@
+from util.VisualizeDataset import VisualizeDataset
+import sys
+from Chapter5.DistanceMetrics import InstanceDistanceMetrics
+from Chapter5.DistanceMetrics import PersonDistanceMetricsNoOrdering
+from Chapter5.DistanceMetrics import PersonDistanceMetricsOrdering
+from Chapter5.Clustering import NonHierarchicalClustering
+from Chapter5.Clustering import HierarchicalClustering
+import copy
+import pandas as pd
+import matplotlib.pyplot as plot
+import util.util as util
+from scipy.cluster.hierarchy import dendrogram
+
+# Of course we repeat some stuff from Chapter 3, namely to load the dataset
+
+DataViz = VisualizeDataset()
+
+dataset = pd.read_csv('./intermediate_datafiles/chapter4_result.csv', index_col=0)
+dataset.index = dataset.index.to_datetime()
+
+# First let us use non hierarchical clustering.
+
+clusteringNH = NonHierarchicalClustering()
+
+# Let us look at k-means first.
+'''
+k_values = range(2, 10)
+silhouette_values = []
+#
+## Do some initial runs to determine the right number for k
+#
+print '===== kmeans clustering ====='
+for k in k_values:
+    print 'k = ', k
+    dataset_cluster = clusteringNH.k_means_over_instances(copy.deepcopy(dataset), ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], k, 'default', 20, 10)
+    silhouette_score = dataset_cluster['silhouette'].mean()
+    print 'silhouette = ', silhouette_score
+    silhouette_values.append(silhouette_score)
+
+plot.plot(k_values, silhouette_values, 'b-')
+plot.xlabel('k')
+plot.ylabel('silhouette score')
+plot.ylim([0,1])
+plot.show()
+
+# And run the knn with the highest silhouette score
+
+k = 5
+
+dataset_knn = clusteringNH.k_means_over_instances(copy.deepcopy(dataset), ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], k, 'default', 50, 50)
+DataViz.plot_clusters_3d(dataset_knn, ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], 'cluster', ['label'])
+DataViz.plot_silhouette(dataset_knn, 'cluster', 'silhouette')
+util.print_latex_statistics_clusters(dataset_knn, 'cluster', ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], 'label')
+del dataset_knn['silhouette']
+
+
+k_values = range(2, 10)
+silhouette_values = []
+
+# Do some initial runs to determine the right number for k
+
+print '===== k medoids clustering ====='
+for k in k_values:
+    print 'k = ', k
+    dataset_cluster = clusteringNH.k_medoids_over_instances(copy.deepcopy(dataset), ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], k, 'default', 20, n_inits=10)
+    silhouette_score = dataset_cluster['silhouette'].mean()
+    print 'silhouette = ', silhouette_score
+    silhouette_values.append(silhouette_score)
+
+plot.plot(k_values, silhouette_values, 'b-')
+plot.ylim([0,1])
+plot.xlabel('k')
+plot.ylabel('silhouette score')
+plot.show()
+
+# And run k medoids with the highest silhouette score
+
+k = 2 
+
+dataset_kmed = clusteringNH.k_medoids_over_instances(copy.deepcopy(dataset), ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], k, 'default', 20, n_inits=50)
+DataViz.plot_clusters_3d(dataset_kmed, ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], 'cluster', ['label'])
+DataViz.plot_silhouette(dataset_kmed, 'cluster', 'silhouette')
+util.print_latex_statistics_clusters(dataset_kmed, 'cluster', ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], 'label')
+
+# And the hierarchical clustering is the last one we try
+'''
+clusteringH = HierarchicalClustering()
+
+k_values = range(2, 10)
+silhouette_values = []
+
+def plot_dendrogram(dataset, linkage, k):
+    sys.setrecursionlimit(40000)
+    plot.title('Hierarchical Clustering Dendrogram')
+    plot.xlabel('time points')
+    plot.ylabel('distance')
+    times = dataset.index.strftime('%H:%M:%S')
+    dendrogram(linkage,truncate_mode='lastp',p=k, show_leaf_counts=True, leaf_rotation=45.,leaf_font_size=8.,show_contracted=True, labels=times)
+    plot.show()
+
+# Do some initial runs to determine the right number for the maximum number of clusters.
+
+print '===== agglomaritive clustering ====='
+
+for k in k_values:
+    print 'k = ', k
+    dataset_cluster, l = clusteringH.agglomerative_over_instances(copy.deepcopy(dataset), ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], k, 'euclidean', use_prev_linkage=True, link_function='ward')
+    silhouette_score = dataset_cluster['silhouette'].mean()
+    print 'silhouette = ', silhouette_score
+    silhouette_values.append(silhouette_score)
+  #  if k == k_values[0]:
+    #plot_dendrogram(dataset_cluster, l, k)
+
+plot.plot(k_values, silhouette_values, 'b-')
+plot.ylim([0,1])
+plot.xlabel('max number of clusters')
+plot.ylabel('silhouette score')
+plot.show()
+
+silhouette_values = []
+print '===== parameter testing ====='
+
+print '2p = ', 0
+dataset_cluster, l = clusteringH.agglomerative_over_instances(copy.deepcopy(dataset), ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], 10, 'manhattan', use_prev_linkage=False, link_function='complete')
+silhouette_score = dataset_cluster['silhouette'].mean()
+print 'silhouette = ', silhouette_score
+silhouette_values.append(silhouette_score)
+plot_dendrogram(dataset_cluster, l, 16)
+for p in range(1,10):
+    print '2p = ', p
+    dataset_cluster, l = clusteringH.agglomerative_over_instances(copy.deepcopy(dataset), ['gyr_phone_x', 'gyr_phone_y', 'gyr_phone_z'], 10, 'minkowski', p = p*0.5, use_prev_linkage=False, link_function='complete')
+    silhouette_score = dataset_cluster['silhouette'].mean()
+    print 'silhouette = ', silhouette_score
+    silhouette_values.append(silhouette_score)
+    plot_dendrogram(dataset_cluster, l, 16)
+
+plot.plot(range(10), silhouette_values, 'b-')
+plot.ylim([0,1])
+plot.xlabel('p used for minkowski')
+plot.ylabel('silhouette score')
+plot.show()
+
+
